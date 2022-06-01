f6ed70c4bab48940d069bf6fb140e0aaef9f956d
Distractic <distractic@outlook.fr>
2022-06-01 18:59:15 +0200

    doc: Add documentation dokka

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     kotlin("jvm") version "1.6.21"
     kotlin("plugin.serialization") version "1.6.21"
-    id("com.github.johnrengelman.shadow") version "7.1.2"
+    id("org.jetbrains.dokka") version "1.6.21"
 }
 
 val projectVersion: String by project
@@ -108,11 +108,14 @@ tasks {
         kotlinOptions.jvmTarget = "17"
     }
 
-    build {
-        dependsOn(shadowJar)
+    val dokkaOutputDir = "${rootProject.projectDir}/dokka"
+
+    clean {
+        delete(dokkaOutputDir)
     }
-    shadowJar {
-        archiveFileName.set("${project.name}.jar")
-        destinationDirectory.set(file("build"))
+
+    dokkaHtml.configure {
+        dependsOn(clean)
+        outputDirectory.set(file(dokkaOutputDir))
     }
 }
