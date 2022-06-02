1b8c033fa8c4f3c4d6dc6f7d411baa3d3f7542d8
Distractic <distractic@outlook.fr>
2022-06-02 23:20:08 +0200

    chore: Remove useless configuration for buildSrc

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -9,11 +9,4 @@ repositories {
 dependencies {
     implementation(gradleApi())
     implementation(localGroovy())
-    implementation(kotlin("gradle-plugin", version = "1.6.21"))
-}
-
-tasks {
-    withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
-        kotlinOptions.jvmTarget = "17"
-    }
 }
